0a53078df87070e9f3c109e24aa5892b614d6751
Kelvin <kelvingiovannno@gmail.com>
2023-10-19 21:10:04 +0700

    Initial Project

---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className='w-full bg-secondary flex h-16'>
+        <div>
+            Logo
+        </div>
+        <div>Home</div>
+        <div>About Me</div>
+        <div>Portfolio</div>
+        <div>Services</div>
+        <div>Experience</div>
+        <div>Contact</div>
+    </div>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#212121',
+        secondary: '#616161',
+      },
+    },
+  },
+  plugins: [],
+}
+
